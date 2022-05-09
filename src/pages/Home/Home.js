44519988058d3663583f4e42787e804ff0044be9
aditya19xx <turjaadityadas@gmail.com>
2022-05-09 23:57:09 +0600

    implemented search by rocket name

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Row } from "react-bootstrap";
+import { Button, Form, Nav, Navbar, Row } from "react-bootstrap";
 import Data from "../Data/Data";
-import Navigation from "../Navigation/Navigation";
 
 const Home = () => {
   const [dataApi, setDataApi] = useState([]);
@@ -16,9 +15,42 @@ const Home = () => {
         setDisplayData(data);
       });
   }, []);
+
+  const handleSearch = e => {
+    const searchText = e.target.value;
+    const searchResult = dataApi.filter(d => d.rocket.rocket_name.toLowerCase().includes(searchText.toLowerCase()));
+    setDisplayData(searchResult);
+}
   return (
     <>
-      <Navigation></Navigation>
+      <navigation>
+      <div className="body">
+             <Navbar bg="dark" variant="dark" expand="lg">
+                    <Navbar.Brand href="#">Technext test</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="mr-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            <Nav.Link href="#action1">Home</Nav.Link>
+                            <Nav.Link href="#action2">About</Nav.Link>
+                        </Nav>
+                        <Form className="d-flex">
+                            <input
+                            onChange={ handleSearch }
+                                type="search"
+                                placeholder="Search by Rocket name"
+                                className="mr-2"
+                                aria-label="Search"
+                            />
+                            <Button  variant="outline-success">Search</Button>
+                        </Form>
+                    </Navbar.Collapse>
+                </Navbar>
+        </div>
+      </navigation>
       <div className="w-75 mx-auto mt-5">
         <Row xs={1} md={2} className="g-4">
           {
@@ -32,5 +64,6 @@ const Home = () => {
     </>
   );
 };
-
+ 
 export default Home;
+
