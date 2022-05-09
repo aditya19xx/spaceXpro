aeb0650afd997e8293f61812d54f6f417f4e7899
aditya19xx <turjaadityadas@gmail.com>
2022-05-09 23:10:37 +0600

    fetched api

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navigation from '../Navigation/Navigation';
 
 const Home = () => {
+    const [dataApi, setDataApi] = useState([]);
+    const [displayData, setDisplayData] = useState([]);
+
+    useEffect(()=>{
+         fetch('https://api.spacexdata.com/v3/launches')
+         .then(res => res.json())
+         .then(data => {
+             console.log(data);
+             setDataApi(data);
+             setDisplayData(data);
+         })
+    }, [])
     return (
-        <div>
+        <>
             <Navigation></Navigation>
             <h2>This is Home</h2>
-        </div>
+        </>
     );
 };
 
