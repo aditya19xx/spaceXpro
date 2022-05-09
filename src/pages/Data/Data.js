07782dc77277961964358d357c754f238f1cfb0c
aditya19xx <turjaadityadas@gmail.com>
2022-05-09 23:27:34 +0600

    data showed on ui

---
+++ src/pages/Data/Data.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Card, Col } from 'react-bootstrap';
+
+const Data = (props) => {
+    const { mission_name,rocket, rocket_name, links, mission_patch, launch_date_local, upcoming, launch_year,launch_success,} = props.data;
+
+    return (
+        <div>
+            <Col>
+                <Card className="bg-dark card-color rounded-3 shadow-lg p-3 mb-5  rounded"  >
+                    <Card.Img className="card-img" variant="top" src={links.mission_patch} />
+                    <Card.Body className="text-white">
+                        <Card.Title>
+                            <h1>{rocket.rocket_name}</h1>
+                            <h6>{mission_name}</h6>
+
+                        </Card.Title>
+                        <Card.Text>
+                           
+                           <h5> <span className="">Launch Date:</span> {launch_date_local} </h5>
+                           <h5> Launch Year: {launch_year}</h5>
+                           
+                           <h5> Launch Status: {launch_success} </h5>
+                           <h5> Is it upcoming? {upcoming} </h5>
+                           
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </div>
+    );
+};
+
+export default Data;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,25 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import Navigation from '../Navigation/Navigation';
+import React, { useEffect, useState } from "react";
+import { Row } from "react-bootstrap";
+import Data from "../Data/Data";
+import Navigation from "../Navigation/Navigation";
 
 const Home = () => {
-    const [dataApi, setDataApi] = useState([]);
-    const [displayData, setDisplayData] = useState([]);
+  const [dataApi, setDataApi] = useState([]);
+  const [displayData, setDisplayData] = useState([]);
 
-    useEffect(()=>{
-         fetch('https://api.spacexdata.com/v3/launches')
-         .then(res => res.json())
-         .then(data => {
-             console.log(data);
-             setDataApi(data);
-             setDisplayData(data);
-         })
-    }, [])
-    return (
-        <>
-            <Navigation></Navigation>
-            <h2>This is Home</h2>
-        </>
-    );
+  useEffect(() => {
+    fetch("https://api.spacexdata.com/v3/launches")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setDataApi(data);
+        setDisplayData(data);
+      });
+  }, []);
+  return (
+    <>
+      <Navigation></Navigation>
+      <div className="w-75 mx-auto mt-5">
+        <Row xs={1} md={2} className="g-4">
+          {
+              displayData.map(dt => <Data
+              key={'flight_number'}
+              data={dt}
+              ></Data>)
+          }
+        </Row>
+      </div>
+    </>
+  );
 };
 
 export default Home;
